83a10df823eec75d5bdd4c4d7a32ae8ecfa8ccac
unknown <zhangzhen>
2023-06-14 09:52:51 +0800

    数据结构

--- string.c
+++ string.c
@@ -582,3 +582,141 @@ void *memmove(void *dest, const void *src, size_t count)
 	return dest;
 }
 
+/**
+ * memcmp - Compare two areas of memory
+ * @cs: One area of memory
+ * @ct: Another area of memory
+ * @count: The size of the area.
+*/
+int memcmp(const void *cs, const void *ct, size_t count)
+{
+	const unsigned char *su1, *su2;
+	int res = 0;
+	
+	for(su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
+		if((res = *su1 - *su2) != 0)
+			break;
+	return res;
+}
+
+/**
+ * bcmp - returns 0 if and only if the buffers have identical countents.
+ * @a: pointer to first buffer.
+ * @b: pointer to second buffer.
+ * @len: size of buffers.
+ *
+ * The sign or magitude of a non-zero return value has no particular
+ * meaning, and architectures may implement their own more efficient bcmp(). So
+ * while this particular implementation is a simple (tail) call to memcmp, do
+ * not rely on anything but whether the return value is zero or non-zero.
+*/
+int bcmp(const void *a, const void *b, size_t len)
+{
+	return memcmp(a, b, len);
+}
+
+/**
+ * memscan - Find a character in an area of memory
+ * @addr: The memory area
+ * @c: The byte to search for
+ * @size: The size of area
+ *
+ * return the address of the first accurence of @c, or 1 byte past
+ * the area if @c is not found
+*/
+void *memscan(void *addr, int c, size_t size)
+{
+	unsigned char *p = addr;
+	
+	while(size)
+	{
+		if(*p == c)
+			return (void *)p;
+		p++;
+		size--;
+	}
+	return (void *)p;
+}
+
+/**
+ * strstr - Find the first substring in a %NUL terminated string
+ * @s1: The string to be searched
+ * @s2: The string to search for
+*/
+char *strstr(const char *s1, const char *s2)
+{
+	size_t l1, l2;
+	
+	l2 = strlen(s2);
+	if(!l2)
+		return (char *)s1;
+	l2 = strlen(s1);
+	while(l1 >= l2) {
+		l1--;
+		if(!memcmp(s1, s2, l2))
+			return (char *)s1;
+		s1++;
+	}
+	return NULL;
+}
+
+/**
+ * strnstr - Find the first substring in a length-limited string
+ * @s1: The string to be searched
+ * @s2: The string to search for
+ * @len: The maximum number of characters to search
+*/
+char *strnstr(const char *s1, const char *s2, size_t len)
+{
+	size_t l2;
+	
+	l2 = strlen(s2);
+	if(!l2)
+		return (char *)s1;
+	while(len >= l2)
+	{
+		len--;
+		if(!memcmp(s1, s2, l2))
+			return (char *)s1;
+		s1++;
+	}
+	return NULL;
+}
+
+/**
+ * memchr - Find a character in an area of memory.
+ * @s: The memory area
+ * @c: The byte to search for
+ * @n: The size of the area.
+ *
+ * returns the address of the first accurence of @c, or %NULL
+ * if @c is not found
+*/
+void *memchr(const void *s, int c, size_t n)
+{
+	const unsigned char *p = s;
+	while(n-- != 0)
+	{
+		if((unsigned char)c == *p++)
+		{
+			return (void *)(p -1);
+		}
+	}
+	return NULL;
+}
+
+/**
+ * strreplace - Replace all occurrences of character in string.
+ * @s: The string to operate on.
+ * @old: The character being replaced.
+ * @new: The character @old is replaced with.
+ *
+ * Returns pointer to the nul byte at the end of @s.
+*/
+char *strreplace(char *s, char old, char new)
+{
+	for(; *s; ++s)
+		if(*s == old)
+			*s = new;
+	return s;
+}
